b3f8707f709a4bba5a38b0a8c246517ae0a4841c
MentalWombat <e.ochneva@gmail.com>
2017-10-19 16:02:34 +0300

    all files added

---
+++ bmc.js
@@ -0,0 +1,43 @@
+$(function() {
+	$("form").submit(function() {
+		var day = $("#day").val();
+		if (day == "") {
+			$("#answer").text("Try again and don't forget to enter a number.");
+			return false;
+		}
+		else {
+			switch (day % 7) {
+				case 0:
+					$("#answer").text("It is Blood Moon.").css({"color":"rgb(207,29,22)"});
+					return false;
+					break;
+				case 1:
+					$("#answer").text("It's the 1st day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+					break;
+				case 2:
+					$("#answer").text("It's the 2nd day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+					break;
+				case 3:
+					$("#answer").text("It's the 3rd day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+					break;
+				case 4:
+					$("#answer").text("It's the 4th day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+					break;
+				case 5:
+					$("#answer").text("It's the 5th day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+					break;
+				case 6:
+					$("#answer").text("It's the 6th day of the week.").css({"color":"rgb(213,204,167)"});
+					return false;
+			}
+		}
+	});
+	$("#day").keydown(function(){
+		$("#answer").html("&nbsp;");
+	});
+});
